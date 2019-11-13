3c6f0bad0e92da6199a0c8e0ce6ce613ff4e4f60
kains praveen <kainspraveen@icloud.com>
2019-11-13 15:14:43 +0530

    Update data-extractor.py

--- data-extractor.py
+++ data-extractor.py
@@ -3,7 +3,6 @@ import time
 import pandas as pd
 class Hardware:
     def __init__(self):
-        self.file=None
         #self.net=ps.net_io_counters()[1]-ps.net_io_counters()[0]
         self.frame=[]
         #self.file.write('timestamp  ,   cpu ,   ram ,   disk    ,   network\n')
@@ -22,22 +21,11 @@ class Hardware:
         network=network[len(network)-2:]
         self.net=networks[1]-networks[0]
         disk=str(disk)
-        if(save==1):
-            self.file.write(str(t)+"    ,   "+str(cpu)+"   ,    "+ram[1:]+"   ,    "+disk[5:]+"   ,   "+network+"\n")
-            self.frame.append([str(t),str(cpu),ram[1:],disk[5:],network])
-        else:
-            self.frame.append([str(t),str(cpu),ram[1:],disk[5:],network])
-    def frameReturn(self):
-        #print("**")
-        print(pd.DataFrame(self.frame))
-        return pd.DataFrame(self.frame)
+        self.frame.append([str(t),str(cpu),ram[1:],disk[5:],network])
 
     def helper(self,save=1):
         start=time.time()
         #save=int(input("want a CSV or not?\n"))
-        if(save==1):
-            self.file=open('data.csv','a')
-            self.file.write('timestamp  ,   cpu ,   ram ,   disk    ,   network\n')
         count=0
         while(True):
             if(time.time()-start>=1):
@@ -48,9 +36,9 @@ class Hardware:
                 start=time.time()
             if(count>=20):
                 break
-        if(save==1):
-            self.file.close()
         #print(pd.DataFrame(self.frame))
+        if(save==1):
+            pd.DataFrame(self.frame).to_csv('data.csv')
         return pd.DataFrame(self.frame)
 
 
