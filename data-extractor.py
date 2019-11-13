97ac288f9d6fad66fddb2909588642b18e73cf49
kains praveen <kainspraveen@icloud.com>
2019-11-13 14:55:41 +0530

    updated data-extractor

--- data-extractor.py
+++ data-extractor.py
@@ -1,47 +1,61 @@
 import psutil as ps
 import time
-import os
+import pandas as pd
 class Hardware:
     def __init__(self):
-        if os.path.exists('data.csv'):
-            self.file=open('data.csv','a')
-        else:
-            self.file=open('data.csv','w')
-            self.file.write('timestamp,cpu,ram,disk,network\n')
-        self.net=0
-    def tracker(self):
+        self.file=None
+        #self.net=ps.net_io_counters()[1]-ps.net_io_counters()[0]
+        self.frame=[]
+        #self.file.write('timestamp  ,   cpu ,   ram ,   disk    ,   network\n')
+    def tracker(self,save=1):
         t=time.ctime(time.time())
         cpu=ps.cpu_percent()
         m=ps.virtual_memory()
-        ram=m[2]
-        d=ps.swap_memory()
-        disk=d[2]
+        ram=m[2]*10
+        ram=str(ram)
+        d=ps.disk_io_counters()
+        disk=d[1]-d[0]
         #network=ps.sensors_fans()
         networks=ps.net_io_counters()
-        network=networks[1]//(1024*1024)-self.net
-        self.net=networks[1]//(1024*1024)
-        self.file.write(str(t)+","+str(cpu)+","+str(ram)+","+str(disk)+","+str(network)+"\n")
-    """def writer(self):
-        try:
-            thread.start_new_thread(self.tracker())
-        except:
-            print("error exception")
-        while 1:
-            pass"""
+        network=networks[1]-networks[0]
+        network=str(network)
+        network=network[len(network)-2:]
+        self.net=networks[1]-networks[0]
+        disk=str(disk)
+        if(save==1):
+            self.file.write(str(t)+"    ,   "+str(cpu)+"   ,    "+ram[1:]+"   ,    "+disk[5:]+"   ,   "+network+"\n")
+            self.frame.append([str(t),str(cpu),ram[1:],disk[5:],network])
+        else:
+            self.frame.append([str(t),str(cpu),ram[1:],disk[5:],network])
+    def frameReturn(self):
+        #print("**")
+        #print(pd.DataFrame(self.frame))
+        return pd.DataFrame(self.frame)
+
+
+
+
 def main():
     start=time.time()
     obj=Hardware()
-    print("**")
+
+    save=int(input("want a CSV or not?\n"))
+    if(save==1):
+        obj.file=open('data.csv','a')
+        obj.file.write('timestamp  ,   cpu ,   ram ,   disk    ,   network\n')
     count=0
     while(True):
         if(time.time()-start>=1):
             count+=1
             print(count)
-            obj.tracker()
+            obj.tracker(save)
             print("*")
             start=time.time()
         if(count>=20):
             break
-    obj.file.close()
+    obj.frameReturn()
+    if(save==1):
+        obj.file.close()
+    #call dataFrameGenerator() function to get pandas dataframe object
 if __name__ == '__main__':
     main()
