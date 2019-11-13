40fd75d8abd039e20ee47ec5de97b65d7a4a1e14
kains praveen <kainspraveen@icloud.com>
2019-11-13 15:05:26 +0530

    Update data-extractor.py

--- data-extractor.py
+++ data-extractor.py
@@ -7,7 +7,7 @@ class Hardware:
         #self.net=ps.net_io_counters()[1]-ps.net_io_counters()[0]
         self.frame=[]
         #self.file.write('timestamp  ,   cpu ,   ram ,   disk    ,   network\n')
-    def tracker(self,save=1):
+    def tracker(self,save):
         t=time.ctime(time.time())
         cpu=ps.cpu_percent()
         m=ps.virtual_memory()
@@ -29,33 +29,36 @@ class Hardware:
             self.frame.append([str(t),str(cpu),ram[1:],disk[5:],network])
     def frameReturn(self):
         #print("**")
+        print(pd.DataFrame(self.frame))
+        return pd.DataFrame(self.frame)
+
+    def helper(self,save=1):
+        start=time.time()
+        #save=int(input("want a CSV or not?\n"))
+        if(save==1):
+            self.file=open('data.csv','a')
+            self.file.write('timestamp  ,   cpu ,   ram ,   disk    ,   network\n')
+        count=0
+        while(True):
+            if(time.time()-start>=1):
+                count+=1
+                print(count)
+                self.tracker(save)
+                print("*")
+                start=time.time()
+            if(count>=20):
+                break
+        if(save==1):
+            self.file.close()
         #print(pd.DataFrame(self.frame))
         return pd.DataFrame(self.frame)
 
 
 
-
 def main():
-    start=time.time()
-    obj=Hardware()
 
-    save=int(input("want a CSV or not?\n"))
-    if(save==1):
-        obj.file=open('data.csv','a')
-        obj.file.write('timestamp  ,   cpu ,   ram ,   disk    ,   network\n')
-    count=0
-    while(True):
-        if(time.time()-start>=1):
-            count+=1
-            print(count)
-            obj.tracker(save)
-            print("*")
-            start=time.time()
-        if(count>=20):
-            break
-    obj.frameReturn()
-    if(save==1):
-        obj.file.close()
+    obj=Hardware()
+    obj.helper()
     #call dataFrameGenerator() function to get pandas dataframe object
 if __name__ == '__main__':
     main()
